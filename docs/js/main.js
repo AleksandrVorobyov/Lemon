7fe9b48d587f5687b7361985507186587e130de0
Alex <sasha89253215539@yandex.ru>
2021-02-13 15:58:00 +0300

    desctop landing done

---
+++ docs/js/main.js
@@ -0,0 +1,4 @@
+const navSearchBtn = document.getElementById('nav-search-btn');
+navSearchBtn.addEventListener('click', function() {
+    this.classList.add('nav__btn--active');
+})
---
+++ src/js/carousel.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(".header-carousel").owlCarousel({
+        margin: 10,
+        items: 1,
+        nav: false,
+        dots: true,
+        mouseDrag: true,
+        smartSpeed: 3000
+    });
+});
+
+$(document).ready(function(){
+    $(".post-mini__carousel").owlCarousel({
+        margin: 10,
+        items: 1,
+        nav: false,
+        dots: true,
+        mouseDrag: true,
+        smartSpeed: 2000
+    });
+});
