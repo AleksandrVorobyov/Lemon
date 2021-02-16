6db6c7d4be2c1d3a9aee45885797d61d9076cd6d
Alex <sasha89253215539@yandex.ru>
2021-02-16 08:59:07 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,25 @@
-const navSearchBtn = document.getElementById('nav-search-btn');
-navSearchBtn.addEventListener('click', function() {
-    this.classList.add('nav__btn--active');
-})
+if (window.innerWidth > 1150) {
+    const navSearchBtn = document.getElementById('nav-search-btn');
+    navSearchBtn.addEventListener('click', function() {
+        this.classList.add('nav__btn--active');
+    })
+}
+
+const navDropdownTitle = document.getElementById('dropdown-title');
+const navDropdownLinks = document.getElementById('dropdown-links');
+
+navDropdownTitle.addEventListener('mouseover', function() {
+    navDropdownLinks.classList.add('nav__dropdown-wrap--active')
+});
+
+navDropdownLinks.addEventListener('mouseover', function() {
+    navDropdownLinks.classList.add('nav__dropdown-wrap--active')
+});
+
+navDropdownTitle.addEventListener('mouseout', function() {
+    navDropdownLinks.classList.remove('nav__dropdown-wrap--active')
+});
+
+navDropdownLinks.addEventListener('mouseout', function() {
+    navDropdownLinks.classList.remove('nav__dropdown-wrap--active')
+});
