7827b8e625700ba1519cbf0053fc32f7302224d6
Alex <sasha89253215539@yandex.ru>
2021-02-18 17:37:47 +0300

    mobile web done

--- src/js/main.js
+++ src/js/main.js
@@ -23,3 +23,33 @@ navDropdownTitle.addEventListener('mouseout', function() {
 navDropdownLinks.addEventListener('mouseout', function() {
     navDropdownLinks.classList.remove('nav__dropdown-wrap--active')
 });
+
+window.onscroll = function showHeader() {
+    if (window.innerWidth < 720) {
+        const nav = document.getElementById('nav');
+        if (window.pageYOffset > 200) {
+            nav.classList.add('nav--fixed');
+        } else {
+            nav.classList.remove('nav--fixed');
+        }
+    }
+}
+
+const burger = document.getElementById('nav-burger');
+const sidebar = document.getElementById('sidebar');
+const body = document.querySelector('body');
+
+burger.addEventListener('click', function() {
+    this.classList.toggle('nav__burger--active');
+    sidebar.classList.toggle('sidebar--active');
+    body.classList.toggle('body--hidden');
+})
+
+const sidebarBtn = document.querySelectorAll('[data-sidebar-btn]');
+sidebarBtn.forEach( function(item) {
+    item.addEventListener('click', function() {
+        this.classList.toggle('sidebar__table-col-link--active');
+        let sidebarTable = this.closest('.sidebar__table-col-item');
+        sidebarTable.querySelector('.sidebar__table-sublinks').classList.toggle('sidebar__table-sublinks--active');
+    })
+})
